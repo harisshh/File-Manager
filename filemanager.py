86ee7ece2ed85a501f8633db720ac7b62e0bd9a3
Harish <66348607+harisshh@users.noreply.github.com>
2022-02-18 01:59:21 +0530

    Add files via upload

--- filemanager.py
+++ filemanager.py
@@ -1,16 +1,16 @@
 import os
 import shutil
 
-#print(os.getcwd())
-
+# Get path from the user
 path = input("Enter the Path: ")
 files = os.listdir(path)
 for file in files:
+    # Split the filename and extension
     filename,extension = os.path.splitext(file)
+    # Remove the . from final extension
     extension = extension[1:]
     if os.path.exists(path+ '/' + extension):
         shutil.move(path+'/'+file, path+'/'+extension+'/'+file)
     else:
         os.mkdir(path+'/'+extension)
         shutil.move(path+'/'+file, path+'/'+extension+'/'+file)
-        
